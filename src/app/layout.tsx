ec4c1b110c4f84cd0715775a796e47edc17a00d4
Ben Halverson <benhalverson33@gmail.com>
2024-04-02 03:07:26 -0700

    layout component updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Toaster } from 'react-hot-toast'; 
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NavBar />
+        {children}
+        <Toaster />
+        </body>
     </html>
   );
 }
+
+const NavBar = () => {
+  return (<div className="navbar bg-base-100">
+    <div className="flex-1">
+      <Link href="/" className="btn btn-ghost text-xl">Stripe for SaaS</Link>
+    </div>
+    <div className="flex-none">
+      <ul className="menu menu-horizontal px-1">
+        <li >
+          <Link href="/" className="btn">Home</Link>
+        </li>
+        <li >
+          <Link href="/photos" className="btn mx-3">Photos</Link>
+        </li>
+        <li >
+          <Link href="/user" className="btn mx-3">User Auth</Link>
+        </li>
+      </ul>
+    </div>
+  </div>)
+};
